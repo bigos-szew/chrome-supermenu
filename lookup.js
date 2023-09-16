ab327f254a6059d1bc93be65e8d3ff2cb332846a
bigos-szew <m.lanocha@protonmail.com>
2023-09-16 10:51:24 +0200

    change popup look

--- lookup.js
+++ lookup.js
@@ -1,17 +1,13 @@
 var searchPopup = document.createElement("div");
-searchPopup.innerHTML = '<div class="meal-details-popper" data-popper-reference-hidden="false" data-popper-escaped="false" data-popper-placement="left" style="position: absolute; inset: 0px 0px auto auto; transform: translate(-669px, 668px);"> <div class="meal-details"> <div id="details-exit" style="position: absolute; top: 16px; right: 18px; width: 30px; cursor: pointer;"><i class="fal fa-times"></i></div><div class="details-name spacer-bottom-24"> <p class="text-h3 text-h3--meal-name">Podaj składnik</p><input type="text" placeholder="koperek" id="ingredient-input"> <button id="ingredient-submit">Szukaj</button> </div></div>';
+searchPopup.innerHTML = '<div class="meal-details-popper" data-popper-reference-hidden="false" data-popper-escaped="false" data-popper-placement="left" style="position: absolute; inset: 0px 0px auto auto; transform: translate(-669px, 668px);"> <div class="meal-details"> <div id="details-exit" style="position: absolute; top: 16px; right: 18px; width: 30px; cursor: pointer;"><i class="fal fa-times"></i></div><div class="details-name spacer-bottom-12"> <p class="text-h3 text-h3--meal-name spacer-bottom-12">Wyszukiwarka składników</p><div class="row"> <div class="col-12 spacer-bottom-8"> <div class="form-input is-required"> <div class="input-label "> <label class="is-required"><span>Składnik</span></label> </div><div class="position-relative"> <input class="is-required" id="ingredient-input" type="text" placeholder="koperek" value=""> </div></div></div></div><button id="ingredient-submit">Szukaj</button> </div></div>';
 searchPopup.id = "search-popup";
-
-const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 const showSearchPopup = () => {
     document.getElementsByClassName("card-header")[0].append(searchPopup);
     document.getElementById("details-exit").onclick = () => { document.getElementById("search-popup").remove() };
     document.getElementById("ingredient-submit").onclick = () => {
-        let ingredient = document.getElementById("ingredient-input").value
-        searchIngredient(ingredient)
+        let ingredient = document.getElementById("ingredient-input").value;
+        searchIngredient(ingredient);
     };
 }
 
@@ -22,18 +18,25 @@ const searchIngredient = async (ingredient) => {
         element.click();
 
 
-        let ingredientString = document.getElementsByClassName("details-ingredients")
+        let ingredientString = document.getElementsByClassName("details-ingredients");
         let markSection = ingredientString[ingredientString.length - 1].textContent.toLowerCase().includes(ingredient.toLowerCase());
 
+        let section = element.parentElement.parentElement.parentElement;
         if (markSection) {
-            element.parentElement.parentElement.parentElement.style.backgroundColor = "#ffb7b7";
+            if (section.tagName === 'LI') {
+                section.style.backgroundColor = "#ffb7b7";
+            } else {
+                section.parentElement.style.backgroundColor = "#ffb7b7";
+            }
         } else {
-            element.parentElement.parentElement.parentElement.style.backgroundColor = "#fff";
+            if (section.tagName === 'LI') {
+                section.style.backgroundColor = "#fff";
+            } else {
+                section.parentElement.style.backgroundColor = "#fff";
+            }
         }
-        let exitButton = document.getElementsByClassName("details-exit")
+        let exitButton = document.getElementsByClassName("details-exit");
         exitButton[exitButton.length - 1].click();
-
-        console.log(exitButton.length)
     }
 }
 
@@ -45,5 +48,5 @@ setTimeout(() => {
     buttonPopup.id = "lookup-popup";
     buttonPopup.onclick = showSearchPopup;
 
-    document.getElementById("app").appendChild(buttonPopup)  
+    document.getElementById("app").appendChild(buttonPopup);
 }, "2000");
